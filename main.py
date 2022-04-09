069c2ba157d59cfc87b0168ff9f879d16d68b8c6
Scooney6 <66892347+Scooney6@users.noreply.github.com>
2022-04-08 21:41:02 -0400

    Add Selection and Quick sort

--- main.py
+++ main.py
@@ -7,11 +7,63 @@ temp_binary_insertion_results = []
 temp_merge_results = []
 temp_heap_results = []
 temp_bubble_results = []
+temp_selection_results = []
+temp_quick_results = []
 insertion_results = []
 binary_insertion_results = []
 merge_results = []
 heap_results = []
 bubble_results = []
+selection_results = []
+quick_results = []
+
+
+def partition(start, end, array):
+
+    # Initializing pivot's index to start
+    pivot_index = start
+    pivot = array[pivot_index]
+
+    # This loop runs till start pointer crosses
+    # end pointer, and when it does we swap the
+    # pivot with element on end pointer
+    while start < end:
+
+        # Increment the start pointer till it finds an
+        # element greater than  pivot
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+
+        # Decrement the end pointer till it finds an
+        # element less than pivot
+        while array[end] > pivot:
+            end -= 1
+
+        # If start and end have not crossed each other,
+        # swap the numbers on start and end
+        if(start < end):
+            array[start], array[end] = array[end], array[start]
+
+    # Swap pivot element with element on end pointer.
+    # This puts pivot on its correct sorted place.
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+
+    # Returning end pointer to divide the array into 2
+    return end
+
+# The main function that implements QuickSort
+def quick_sort(start, end, array):
+
+    if (start < end):
+
+        # p is partitioning index, array[p]
+        # is at right place
+        p = partition(start, end, array)
+
+        # Sort elements before partition
+        # and after partition
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
 
 
 def insertion_sort(arr):
@@ -163,6 +215,20 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
+def selection_sort(arr):
+    for i in range(len(arr)):
+
+    # Find the minimum element in remaining
+    # unsorted array
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+
+    # Swap the found minimum element with
+    # the first element
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
 def gen_random_arr(num):
     arr = []
     if num == 0:
@@ -214,6 +280,22 @@ def test_bubble_sort(arr):
     temp_bubble_results.append(round(end-start, 5))
 
 
+def test_selection_sort(arr):
+    sort_arr = arr.copy()
+    start = time.time()
+    selection_sort(sort_arr)
+    end = time.time()
+    temp_selection_results.append(round(end-start, 5))
+
+
+def test_quick_sort(arr):
+    sort_arr = arr.copy()
+    start = time.time()
+    quick_sort(0, len(sort_arr) - 1, sort_arr)
+    end = time.time()
+    temp_quick_results.append(round(end-start, 5))
+
+
 def avgarr(arr):
     temp = 0
     for k in range(len(arr)):
@@ -229,37 +311,47 @@ def lowest(arr):
     return (round(temp, 5))
 
 if __name__ == "__main__":
-    for i in range(8):
-        for j in range(3):
+    for i in range(10):
+        for j in range(1):
             test_arr = gen_random_arr(i)
             test_insertion_sort(test_arr)
             test_binary_insertion_sort(test_arr)
             test_merge_sort(test_arr)
             test_heap_sort(test_arr)
             test_bubble_sort(test_arr)
+            test_selection_sort(test_arr)
+            test_quick_sort(test_arr)
         insertion_results.append(lowest(temp_insertion_results))
         binary_insertion_results.append(lowest(temp_binary_insertion_results))
         merge_results.append(lowest(temp_merge_results))
         heap_results.append(lowest(temp_heap_results))
         bubble_results.append(lowest(temp_bubble_results))
+        selection_results.append(lowest(temp_selection_results))
+        quick_results.append(lowest(temp_quick_results))
         temp_insertion_results = []
         temp_binary_insertion_results = []
         temp_merge_results = []
         temp_heap_results = []
         temp_bubble_results = []
+        temp_selection_results = []
+        temp_quick_results = []
 
     print("Insertion Sort Results: " + str(insertion_results))
     print("Binary Insertion Sort Results: " + str(binary_insertion_results))
     print("Merge Sort Results: " + str(merge_results))
     print("Heap Sort Results: " + str(heap_results))
     print("Bubble Sort Results: " + str(bubble_results))
+    print("Selection Sort Results: " + str(selection_results))
+    print("Quick Sort Results: " + str(quick_results))
 
-    count = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
+    count = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
     plt.plot(count, insertion_results, label="Insertion")
     plt.plot(count, binary_insertion_results, label="Binary Insertion")
     plt.plot(count, merge_results, label="Merge")
     plt.plot(count, heap_results, label="Heap")
     plt.plot(count, bubble_results, label="Bubble")
+    plt.plot(count, selection_results, label="Selection")
+    plt.plot(count, quick_results, label="Quick")
     plt.xlabel("Numbers In List")
     plt.ylabel('Time (s)')
     plt.legend()
